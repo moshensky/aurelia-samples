edeca77394e5fe08949ed041c3c36ae3caa67734
Nikita Moshensky <nikita@moshensky.com>
2015-06-30 11:49:30 +0300

    save

--- src/src/dates/dates.js
+++ src/src/dates/dates.js
@@ -2,11 +2,9 @@
  * Created by moshensky on 6/29/15.
  */
 import {inject} from 'aurelia-framework';
-import {ModalDialog} from '../../modal-dialog/modal-dialog'
 
-@inject(ModalDialog)
+@inject()
 export class Dates {
-  constructor(dialog) {
-    this.dialog = dialog;
+  constructor() {
   }
 }
--- src/src/modal/simple-dialogs/simple-dialogs.js
+++ src/src/modal/simple-dialogs/simple-dialogs.js
@@ -3,20 +3,44 @@ import {Logger} from 'service/logger';
 import {I18N} from 'aurelia-i18next';
 import {inject} from 'aurelia-framework';
 import {Validation} from 'aurelia-validation';
-import {ModalDialog} from '../../modal-dialog/modal-dialog'
+//import {ModalDialog} from '../../modal-dialog/modal-dialog'
 
-@inject(Http, Logger, I18N, Validation, ModalDialog)
+@inject(Http, Logger, I18N, Validation)
 export class SimpleDialogs {
-  constructor(http, logger, i18n, validation, dialog) {
+  constructor(http, logger, i18n, validation) {
     this.http = http;
     this.logger = logger;
     this.i18n = i18n;
     this.validation = validation;
-    this.dialog = dialog;
+  //  this.dialog = dialog;
+  }
+
+  showConfirm() {
+    this.confirmShowing = true;
+    console.log('show confirm');
+  }
+
+  onConfirmOkClick() {
+    console.log('on confirm "ok" click')
+    this.confirmShowing = false;
+  }
+
+  onConfirmCancelClick() {
+    this.confirmShowing = false;
+    console.log('on confirm "cancel" click')
   }
 
   showMessage() {
     console.log('showing...message')
-    this.dialog.showMessage();
+    this.showing = true;
+  }
+
+  closeEventGoesHere(){
+    this.showing = false;
+  }
+
+  someFunctionGoesHere(){
+    console.log('do stuff');
+    this.showing = false;
   }
 }
