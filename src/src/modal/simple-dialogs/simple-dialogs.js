44bcfad0d8c3d7abbad5e3a9fc1d9f085337d0bd
Nikita Moshensky <nikita@moshensky.com>
2015-06-30 15:51:21 +0300

    save

--- src/src/modal/simple-dialogs/simple-dialogs.js
+++ src/src/modal/simple-dialogs/simple-dialogs.js
@@ -1,46 +1,20 @@
-import {Http} from 'service/http';
-import {Logger} from 'service/logger';
-import {I18N} from 'aurelia-i18next';
-import {inject} from 'aurelia-framework';
-import {Validation} from 'aurelia-validation';
-//import {ModalDialog} from '../../modal-dialog/modal-dialog'
-
-@inject(Http, Logger, I18N, Validation)
 export class SimpleDialogs {
-  constructor(http, logger, i18n, validation) {
-    this.http = http;
-    this.logger = logger;
-    this.i18n = i18n;
-    this.validation = validation;
-  //  this.dialog = dialog;
+  constructor() {
+    this.confirmDialogShowing = false;
   }
 
-  showConfirm() {
-    this.confirmShowing = true;
+  showConfirmDialog() {
+    this.confirmDialogShowing = true;
     console.log('show confirm');
   }
 
-  onConfirmOkClick() {
+  onConfirmDialogOkClick() {
     console.log('on confirm "ok" click')
-    this.confirmShowing = false;
+    this.confirmDialogShowing = false;
   }
 
-  onConfirmCancelClick() {
-    this.confirmShowing = false;
+  onConfirmDialogCancelClick() {
+    this.confirmDialogShowing = false;
     console.log('on confirm "cancel" click')
-  }
-
-  showMessage() {
-    console.log('showing...message')
-    this.showing = true;
-  }
-
-  closeEventGoesHere(){
-    this.showing = false;
-  }
-
-  someFunctionGoesHere(){
-    console.log('do stuff');
-    this.showing = false;
   }
 }
